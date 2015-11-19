acb4d2fbaf6b334a87007013cf45672decf8cdf4
lookOnTheBrightSide <nkosindlovu87@gmail.com>
2015-11-19 11:51:47 +0200

    some basic validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :microposts
+	validates :email, presence: true
+	validates :name, presence: true
+	validates :email, uniqueness: true
+	validates :name, uniqueness: true
 end
