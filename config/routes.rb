e2754565d83cdb8cb524e1829ef8efb6efe2a1c7
lookOnTheBrightSide <nkosindlovu87@gmail.com>
2015-11-19 12:00:38 +0200

    generated home and help controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/help'
+
   resources :microposts
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
